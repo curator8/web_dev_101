17090b8ca90c35916ae6d5ff6b996604fb81d740
katie warren <kelizabethw24@gmail.com>
2023-05-31 10:35:15 -0400

    finish first .js file code notes

---
+++ week-1/basic-JS/variables.js
@@ -0,0 +1,11 @@
+let message; //used to be var, has a few differences, but let took over
+message = "Hello";
+
+alert(message);
+
+/*
+never hyphen a name
+can use $ or _ in names
+case sensitive
+no using reserved words like 'return' and 'let'+
+*/
